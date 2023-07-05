04bd21704e28f263e1df6dcf4c93d048e8a7ad65
Robbie213 <sasnarinedeodat407@gmail.com>
2023-07-05 15:38:00 -0400

    UseEffect - Event Listeners Example

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -16,14 +16,11 @@ const CleanupFunction = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-    // console.log("hmm, this is interesting");
-    const intId = setInterval(() => {
-      // console.log("hello from interval");
-    }, 1000);
-    return () => {
-      clearInterval(intId);
-      console.log("cleanup");
+    const someFunc = () => {
+      // some logic
     };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
   }, []);
   return <h1>hello there</h1>;
 };
