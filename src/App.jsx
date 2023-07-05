6719502cdf569baed7b668e7006bcb0ad7c0f42a
Robbie213 <sasnarinedeodat407@gmail.com>
2023-07-05 15:47:03 -0400

    Project Structure - Folder Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function.jsx";
+import Navbar from "./tutorial/04-project-structure/starter/Navbar";
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Navbar />
     </div>
   );
 }
