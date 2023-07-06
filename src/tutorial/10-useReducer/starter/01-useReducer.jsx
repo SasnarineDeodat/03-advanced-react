15a9ae602c05b4302576450ebb3be4b5f66f832f
Robbie213 <sasnarinedeodat407@gmail.com>
2023-07-06 12:24:21 -0400

    Actions and Default State

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -6,10 +6,16 @@ const defaultState = {
   isLoading: false,
 };
 
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
 const reducer = (state, action) => {
   if (action.type === "CLEAR_LIST") {
     return { ...state, people: [] };
   }
+  // return state;
+  throw new Error(`No matching "${action.type}" - action type`);
 };
 
 const ReducerBasics = () => {
@@ -20,10 +26,11 @@ const ReducerBasics = () => {
     // setPeople(newPeople);
   };
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST" });
+    dispatch({ type: CLEAR_LIST });
     // setPeople([]);
   };
   const resetList = () => {
+    dispatch({ type: RESET_LIST });
     // setPeople(data);
   };
   console.log(state);
